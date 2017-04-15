e8ea24ee26ea097773c93534ff4653c16e45969a
Gabriel Costa <gabrielcosta08@gmail.com>
2017-04-15 04:40:23 -0300

    Create tests for RepositoriyService

---
+++ app/src/test/java/io/gabrielcosta/githubpopular/model/RepositoryServiceTest.java
@@ -0,0 +1,43 @@
+package io.gabrielcosta.githubpopular.model;
+
+import io.gabrielcosta.githubpopular.utils.TestUtils;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by gabrielcosta on 15/04/17.
+ */
+
+public class RepositoryServiceTest {
+
+  private RepositoryService service;
+
+  @Before
+  public void before() {
+    service = new RepositoryService(
+        RetrofitBuilder.getInstance().build(TestUtils.WIRE_MOCK_FINAL_URL));
+  }
+
+  @Test
+  public void shouldEvaluateTheCorrectUrlForSearchRepositories() {
+    final String result = service.fetchRepositories("Java", "stars", 1).request().url()
+        .toString();
+    final String expected = String
+        .format("%ssearch/repositories?q=language:Java&sort=stars&page=1",
+            TestUtils.WIRE_MOCK_FINAL_URL);
+
+    Assert.assertEquals("URL query isn't equal", expected, result);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldThrowExceptionWhenLanguageIsNull() {
+    service.fetchRepositories(null, "stars", 1).request().url();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldThrowExceptionWhenLanguageIsEmpty() {
+    service.fetchRepositories("", "stars", 1).request().url();
+  }
+
+}
