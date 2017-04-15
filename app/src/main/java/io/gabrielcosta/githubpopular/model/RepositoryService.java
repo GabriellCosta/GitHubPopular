a5abb371851e4afdb4a96c02f4e28a48b957f012
Gabriel Costa <gabrielcosta08@gmail.com>
2017-04-15 04:39:34 -0300

    Update repositories implementation to validate language

--- app/src/main/java/io/gabrielcosta/githubpopular/model/RepositoryService.java
+++ app/src/main/java/io/gabrielcosta/githubpopular/model/RepositoryService.java
@@ -1,5 +1,7 @@
 package io.gabrielcosta.githubpopular.model;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import io.gabrielcosta.githubpopular.entity.RepositoriesDTO;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -15,9 +17,15 @@ public class RepositoryService {
     service = retrofit.create(RepositoryEndpoint.class);
   }
 
-  public Call<RepositoriesDTO> fetchRepositories(final String language, final String sort,
-      final int page) {
-    return service.search(language, sort, page);
+  public Call<RepositoriesDTO> fetchRepositories(@NonNull final String language,
+      @Nullable final String sort,
+      @Nullable final Integer page) {
+
+    if (language != null && !language.isEmpty()) {
+      return service.search(String.format("language:%s", language), sort, page);
+    } else {
+      throw new IllegalArgumentException("Language should not be null or empty");
+    }
   }
 
 }
