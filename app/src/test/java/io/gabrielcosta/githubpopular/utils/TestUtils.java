248ee6688290336127299e230b13c41e069c3a91
Gabriel Costa <gabrielcosta08@gmail.com>
2017-04-16 16:07:57 -0300

    Create methdos to stub service and read file from resource

--- app/src/test/java/io/gabrielcosta/githubpopular/utils/TestUtils.java
+++ app/src/test/java/io/gabrielcosta/githubpopular/utils/TestUtils.java
@@ -1,5 +1,16 @@
 package io.gabrielcosta.githubpopular.utils;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.matching.UrlPathPattern;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by gabrielcosta on 15/04/17.
  */
@@ -11,5 +22,49 @@ public final class TestUtils {
   public static final String WIRE_MOCK_FINAL_URL = String
       .format("%1$s:%2$s/", WIRE_MOCK_URL, WIRE_MOCK_PORT);
   public static final long DEF_SERVICE_ASYNC_WAIT = 100L;
+
+  private static final String TEST_RESOURCE = "src/test/resources/";
+
+  /**
+   *  Stub a api call
+   * @param url url to be stubed
+   * @param file json to be returned from stubed service
+   * @param status status code of HTTP method
+   */
+  public static void stubService(final String url, final String file, final int status) {
+    WireMock.reset();
+    stubService(urlPathEqualTo(url), file, status);
+  }
+
+  /**
+   *  Stub a api call
+   * @param urlPathPattern urlPathPattern to be stubed
+   * @param file json to be returned from stubed service
+   * @param status status code of HTTP method
+   */
+  public static void stubService(final UrlPathPattern urlPathPattern, final String file,
+      final int status) {
+    stubFor(get(urlPathPattern)
+        .willReturn(aResponse()
+            .withStatus(status)
+            .withBody(TestUtils.readFile(file))));
+  }
+
+  /**
+   * Read file from resource test
+   * @param fileName file name
+   * @return  resourceas string
+   */
+  public static String readFile(final String fileName) {
+    try {
+      return readFile(new File(TEST_RESOURCE, fileName));
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e.getMessage());
+    }
+  }
+
+  private static String readFile(final File file) throws FileNotFoundException {
+    return new Scanner(file).useDelimiter("\\Z").next();
+  }
 
 }
