dc3e09b82de93d70ab1397640caa594106938c4d
Gabriel Costa <gabrielcosta08@gmail.com>
2017-04-20 01:03:10 -0300

    Add configuration change lifecycle
    Restore recyclerview state

--- app/src/main/java/io/gabrielcosta/githubpopular/MainActivity.java
+++ app/src/main/java/io/gabrielcosta/githubpopular/MainActivity.java
@@ -18,11 +18,16 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements RepositoryListView {
 
+  private static final String ITEM_LIST = "ITEM_LIST";
+  private static final String LAYOUT_POSITION = "LAYOUT_POSITION";
+  private static final String PAGE_NUMBER = "PAGE_NUMBER";
+
   private RecyclerView recyclerView;
   private RepositoryListPresenter presenter;
   private ProgressBar progressBar;
   private LinearLayoutManager layout;
   private RepositoryListAdapter adapter;
+  private EndlessRecyclerOnScrollListener listener;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,10 @@ public class MainActivity extends AppCompatActivity implements RepositoryListVie
     setContentView(R.layout.activity_main);
     init();
     configureRecyclerView(recyclerView);
-    presenter.load();
+    if (savedInstanceState == null) {
+      presenter.load();
+
+    }
   }
 
   @Override
@@ -58,17 +66,39 @@ public class MainActivity extends AppCompatActivity implements RepositoryListVie
         new RepositoryService(BuildConfig.HOST_NAME));
   }
 
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putSerializable(ITEM_LIST, new SaveListStateDTO(adapter.getItemList()));
+    outState.putParcelable(LAYOUT_POSITION, layout.onSaveInstanceState());
+    outState.putInt(PAGE_NUMBER, listener.getCurrentPage());
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    if (savedInstanceState != null) {
+      layout.onRestoreInstanceState(savedInstanceState.getParcelable(LAYOUT_POSITION));
+      SaveListStateDTO serializable = (SaveListStateDTO) savedInstanceState
+          .getSerializable(ITEM_LIST);
+      adapter.addItems((List<RepositorieVO>) serializable.getList());
+      listener.setCurrentPage(savedInstanceState.getInt(PAGE_NUMBER));
+      progressBar.setVisibility(View.GONE);
+    }
+  }
+
   private void configureRecyclerView(final RecyclerView recyclerView) {
     recyclerView.setVisibility(View.VISIBLE);
     recyclerView.setAdapter(adapter);
     recyclerView.setLayoutManager(layout);
     recyclerView.addItemDecoration(new DividerItemDecoration(this, layout.getOrientation()));
-    recyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(layout) {
+    listener = new EndlessRecyclerOnScrollListener(layout) {
       @Override
       protected void onLoadMore(int currentPage) {
         presenter.loadPage(currentPage);
         progressBar.setVisibility(View.VISIBLE);
       }
-    });
+    };
+    recyclerView.addOnScrollListener(listener);
   }
 }
