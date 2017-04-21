cecb6da8b3c87fbc2d45946700c3acef20cc99d8
Gabriel Costa <gabrielcosta08@gmail.com>
2017-04-21 13:21:36 -0300

    Create method to verify network connection

---
+++ app/src/main/java/io/gabrielcosta/githubpopular/utils/NetworkUtils.java
@@ -0,0 +1,23 @@
+package io.gabrielcosta.githubpopular.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public final class NetworkUtils {
+
+  private NetworkUtils() {
+
+  }
+
+  public static boolean hasConnection(final Context context) {
+    ConnectivityManager cm =
+        (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+    return activeNetwork != null &&
+        activeNetwork.isConnectedOrConnecting();
+
+  }
+
+}
