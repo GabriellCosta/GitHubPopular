6651d70394d834ea9b58e25540ae933f33655c60
Gabriel Costa <gabrielcosta08@gmail.com>
2017-04-22 18:48:40 -0300

    Create activity for pull list

---
+++ app/src/main/java/io/gabrielcosta/githubpopular/ui/activity/PullListActivity.java
@@ -0,0 +1,12 @@
+package io.gabrielcosta.githubpopular.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import io.gabrielcosta.githubpopular.entity.RepositorieVO;
+
+public class PullListActivity extends AppCompatActivity {
+
+  public static final String EXTRA_REPOSITORY = "EXTRA_REPOSITORY";
+
+  private RepositorieVO repositorieVO;
+
+}
