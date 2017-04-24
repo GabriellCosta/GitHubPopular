277bb7100ac84151472d531797f631089082e239
Gabriel Costa <gabrielcosta08@gmail.com>
2017-04-23 22:11:05 -0300

    Init basic view components

--- app/src/main/java/io/gabrielcosta/githubpopular/ui/activity/PullListActivity.java
+++ app/src/main/java/io/gabrielcosta/githubpopular/ui/activity/PullListActivity.java
@@ -1,9 +1,15 @@
 package io.gabrielcosta.githubpopular.ui.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import io.gabrielcosta.githubpopular.R;
 import io.gabrielcosta.githubpopular.contract.PullListContract;
 import io.gabrielcosta.githubpopular.entity.PullRequestVO;
 import io.gabrielcosta.githubpopular.entity.RepositorieVO;
+import io.gabrielcosta.githubpopular.ui.adapter.PullListAdapter;
 import java.util.List;
 
 public class PullListActivity extends AppCompatActivity implements PullListContract.PullListView {
@@ -11,6 +17,15 @@ public class PullListActivity extends AppCompatActivity implements PullListContr
   public static final String EXTRA_REPOSITORY = "EXTRA_REPOSITORY";
 
   private RepositorieVO repositorieVO;
+  private RecyclerView recyclerView;
+  private PullListAdapter adapter;
+  private LinearLayoutManager layoutManager;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    init();
+  }
 
   @Override
   public void setItems(List<PullRequestVO> items) {
@@ -35,5 +50,11 @@ public class PullListActivity extends AppCompatActivity implements PullListContr
   @Override
   public void setStarts(int stars) {
 
+  }
+
+  private void init() {
+    recyclerView = (RecyclerView) findViewById(R.id.rv_pull);
+    layoutManager = new LinearLayoutManager(this);
+    adapter = new PullListAdapter();
   }
 }
