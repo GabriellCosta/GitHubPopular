0e9a4a28bd83014d8e8aa63336bb73c77b6fc3cb
Gabriel Costa <gabrielcosta08@gmail.com>
2017-04-23 22:04:40 -0300

    Make' activity implements his view contract

--- app/src/main/java/io/gabrielcosta/githubpopular/ui/activity/PullListActivity.java
+++ app/src/main/java/io/gabrielcosta/githubpopular/ui/activity/PullListActivity.java
@@ -1,12 +1,39 @@
 package io.gabrielcosta.githubpopular.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
+import io.gabrielcosta.githubpopular.contract.PullListContract;
+import io.gabrielcosta.githubpopular.entity.PullRequestVO;
 import io.gabrielcosta.githubpopular.entity.RepositorieVO;
+import java.util.List;
 
-public class PullListActivity extends AppCompatActivity {
+public class PullListActivity extends AppCompatActivity implements PullListContract.PullListView {
 
   public static final String EXTRA_REPOSITORY = "EXTRA_REPOSITORY";
 
   private RepositorieVO repositorieVO;
 
+  @Override
+  public void setItems(List<PullRequestVO> items) {
+
+  }
+
+  @Override
+  public void setEmptyList() {
+
+  }
+
+  @Override
+  public void setError() {
+
+  }
+
+  @Override
+  public void setForks(int forks) {
+
+  }
+
+  @Override
+  public void setStarts(int stars) {
+
+  }
 }
