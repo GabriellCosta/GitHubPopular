74ec90899062c002c37f5f772a06ea1eee904c2d
Gabriel Costa <gabrielcosta08@gmail.com>
2017-06-12 08:47:18 -0300

    Add RepositoryListActivity

---
+++ mvvm/src/main/java/io/gabrielcosta/mvvm/RepositoryActivity.kt
@@ -0,0 +1,79 @@
+package io.gabrielcosta.mvvm
+
+import android.arch.lifecycle.LifecycleActivity
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.os.Parcelable
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.ProgressBar
+import android.widget.Toast
+import io.gabrielcosta.common.adapter.RepositoryListAdapter
+import io.gabrielcosta.service.entity.RepositorieVO
+
+private const val LAYOUT_POSITION = "LAYOUT_POSITION"
+
+class RepositoryActivity : LifecycleActivity(), View.OnClickListener {
+
+    private val layoutManager = LinearLayoutManager(this)
+    private val recyclerView: RecyclerView by lazy {
+       val field = findViewById(R.id.rv_repository) as RecyclerView
+        field.adapter = adapter
+        field.addItemDecoration(DividerItemDecoration(this, layoutManager.orientation))
+        field
+    }
+
+    private val liveData: LiveData<List<RepositorieVO>> by lazy { repositoryVM.fetchRepositories() }
+
+    private val progress: ProgressBar by lazy {
+        findViewById(R.id.progressbar_repository) as ProgressBar
+    }
+
+    private val adapter: RepositoryListAdapter by lazy {
+        RepositoryListAdapter(this)
+    }
+
+    private val repositoryVM by lazy {
+        ViewModelProviders.of(this).get(RepositoryViewModel::class.java)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(LAYOUT_POSITION, layoutManager.onSaveInstanceState())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null) {
+            layoutManager.onRestoreInstanceState(
+                    savedInstanceState.getParcelable<Parcelable>(LAYOUT_POSITION))
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_list)
+        recyclerView.layoutManager = layoutManager
+        liveData.observe(this, Observer {
+                    adapter.addItems(it)
+                    progress.visibility = GONE
+                    recyclerView.visibility = VISIBLE
+                })
+        repositoryVM.registerForErrorEvent()
+                .observe(this, Observer {
+                    Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+                })
+    }
+
+    override fun onClick(v: View?) {
+        val position = recyclerView.getChildAdapterPosition(v)
+        val get = liveData.value?.get(position)
+        Toast.makeText(this, get?.name, Toast.LENGTH_LONG).show()
+    }
+}
--- mvvm/src/main/java/io/gabrielcosta/mvvm/RepositoryViewModel.kt
+++ mvvm/src/main/java/io/gabrielcosta/mvvm/RepositoryViewModel.kt
@@ -1,7 +1,47 @@
 package io.gabrielcosta.mvvm
 
-/**
- * Created by gabrielcosta on 11/06/17.
- */
-class RepositoryViewModel {
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+import io.gabrielcosta.service.RepositoryService
+import io.gabrielcosta.service.entity.RepositorieVO
+import io.gabrielcosta.service.entity.RepositoriesDTO
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+private const val TAG = "RepositoryViewModel"
+
+class RepositoryViewModel : ViewModel(), Callback<RepositoriesDTO> {
+
+    private var repositories: MutableLiveData<List<RepositorieVO>> = MutableLiveData()
+    private var error: MutableLiveData<String> = MutableLiveData()
+    private val service by lazy { RepositoryService(BuildConfig.HOST_NAME) }
+    
+    fun fetchRepositories(): LiveData<List<RepositorieVO>> {
+
+        service.fetchRepositories("java", "stars", 1)
+                .enqueue(this)
+
+        return repositories
+    }
+
+    fun registerForErrorEvent(): LiveData<String> {
+        return error
+    }
+
+    override fun onResponse(call: Call<RepositoriesDTO>?, response: Response<RepositoriesDTO>?) {
+        if (response?.isSuccessful != null) {
+            repositories.value = response.body()?.items
+        } else {
+            error.value = response?.message()
+        }
+    }
+
+    override fun onFailure(call: Call<RepositoriesDTO>, t: Throwable) {
+        Log.d(TAG, "Fail to call ${call.request().url()}", t)
+        error.value = "Fail to load RepositoryList"
+    }
+    
 }
